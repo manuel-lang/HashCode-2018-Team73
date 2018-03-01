68dd62ad1d71965785ae6b244a592bd8a9783765
manu183 <manuellang183@gmail.com>
2018-03-01 20:15:20 +0100

    dynamic distance

--- ride.py
+++ ride.py
@@ -2,6 +2,6 @@ class Ride:
     def __init__(self, start_position, target_position, distance, earliest_start, latest_finish):
         self.s_position = start_position
         self.t_position = target_position
-        self.distance = distance
+        self.distance = (target_position[1] - start_position[1]) + (target_position[0] - target_position[0])
         self.earliest_start = earliest_start
         self.latest_finish = latest_finish
