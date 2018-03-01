f2ae0fe89e8fa842fdc1688a4a0f3cc41c218ace
manu183 <manuellang183@gmail.com>
2018-03-01 20:41:34 +0100

    update ride

--- ride.py
+++ ride.py
@@ -1,5 +1,7 @@
 class Ride:
-    def __init__(self, start_position, target_position, earliest_start, latest_finish):
+    def __init__(self, index, start_position, target_position, earliest_start, latest_finish):
+        self.index = index
+        self.valid = true
         self.s_position = start_position
         self.t_position = target_position
         self.distance = (target_position[1] - start_position[1]) + (target_position[0] - target_position[0])
