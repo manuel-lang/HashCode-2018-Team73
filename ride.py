01a1d1a15d65ae2bd7f033e6943b62edb3c11848
manu183 <manuellang183@gmail.com>
2018-03-01 20:17:06 +0100

    removed distance

--- ride.py
+++ ride.py
@@ -1,5 +1,5 @@
 class Ride:
-    def __init__(self, start_position, target_position, distance, earliest_start, latest_finish):
+    def __init__(self, start_position, target_position, earliest_start, latest_finish):
         self.s_position = start_position
         self.t_position = target_position
         self.distance = (target_position[1] - start_position[1]) + (target_position[0] - target_position[0])
