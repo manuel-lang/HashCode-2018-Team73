f6e7c4c393ea40aca1669b7fba99158c86bce8c8
manu183 <manuellang183@gmail.com>
2018-03-01 20:11:07 +0100

    ride constructor

--- ride.py
+++ ride.py
@@ -1,6 +1,7 @@
 class Ride:
-    s_position = (0,0)
-    t_position = (0,0)
-    distance = 0
-    earliest_start = 0
-    latest_finish = 0
+    def __init__(self, start_position, target_position, distance, earliest_start, latest_finish):
+        self.s_position = start_position
+        self.t_position = target_position
+        self.distance = distance
+        self.earliest_start = earliest_start
+        self.latest_finish = latest_finish
