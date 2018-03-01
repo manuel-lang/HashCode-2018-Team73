93148ac1054e182954cefca394b0f55625862ea2
manu183 <manuellang183@gmail.com>
2018-03-01 20:06:36 +0100

    auto and ride

---
+++ auto.py
@@ -0,0 +1,7 @@
+class Auto:
+    position = (0,0)
+    driving = False
+    index = 0
+
+    t_free = 0
+    rides_started = []
---
+++ ride.py
@@ -0,0 +1,6 @@
+class Ride:
+    s_position = (0,0)
+    t_position = (0,0)
+    distance = 0
+    earliest_start = 0
+    latest_finish = 0
