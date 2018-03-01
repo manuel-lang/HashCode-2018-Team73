a5fca6e26f58921d734f56b40804bb4f6c6f81fe
axel <updky@student.kit.edu>
2018-03-01 20:32:57 +0100

    pickRide

--- main.py
+++ main.py
@@ -27,8 +27,26 @@ def read_input():
                 rides.append(Ride((int(values[0]), int(values[1])), (int(values[2]), int(values[3])), int(values[4]), int(values[5])))
         return rides
 
+rides = read_input()
+
 def main():
-    rides = read_input()
+
+def calcDistance(pos1,pos2):
+    return abs(pos1[0]-pos2[0])+abs(pos1[1]-pos2[1])
+
+def pickRide(pos,t): # Position vom Auto
+
+    min_d = 99999999999
+    min_index = -1
+
+    for i in range(0,rides.length):
+        d = rides[i].latest_finish-rides[i].distance-calcDistance(pos,rides[i].s_position)
+
+        if d < min_d:
+            min_d = d
+            min_index = i
+
+    return min_index
 
 if __name__ == "__main__":
     main()
