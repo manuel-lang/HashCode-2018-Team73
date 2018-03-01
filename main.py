47d5a0581022c15edefc52010df123a075ce379b
axel <updky@student.kit.edu>
2018-03-01 20:57:37 +0100

    main loop

--- main.py
+++ main.py
@@ -24,12 +24,31 @@ def read_input():
                 bonus = int(values[4])
                 timesteps = int(values[5])
             else:
-                rides.append(Ride((int(values[0]), int(values[1])), (int(values[2]), int(values[3])), int(values[4]), int(values[5])))
+                rides.append(Ride(idx, (int(values[0]), int(values[1])), (int(values[2]), int(values[3])), int(values[4]), int(values[5])))
         return rides
 
 rides = read_input()
+vehicles = []
 
 def main():
+    for t in range(0, timesteps):
+        removeImpossible(t)
+        for vehicle in vehicles:
+            if !(vehicle.t_free > t):
+                ride_index = pickRide(vehicle.position, t)
+                vehicle.rides_started.append(ride_index)
+                vehicle.t_free = t + calcDistance(pos,rides[ride_index].s_position) + max(0, rides[ride_index].earliest_start - (t + calcDistance(pos,rides[ride_index].s_position))) + rides[ride_index].distance
+                rides[ride_index].valid = False
+                
+
+def generateVehicles():
+    for i in range(0, num_vehicles):
+        vehicles.append(Auto(index = i))
+
+def removeImpossible(t):
+    for i in range(0,rides.length):
+        if(rides[i].latest_finish-rides[i].distance < t):
+            rides[i].valid = False
 
 def calcDistance(pos1,pos2):
     return abs(pos1[0]-pos2[0])+abs(pos1[1]-pos2[1])
@@ -42,9 +61,9 @@ def pickRide(pos,t): # Position vom Auto
     for i in range(0,rides.length):
         d = rides[i].latest_finish-rides[i].distance-calcDistance(pos,rides[i].s_position)
 
-        if d < min_d:
+        if d < min_d and rides[i].valid:
             min_d = d
-            min_index = i
+            min_index = rides[i].index
 
     return min_index
 
