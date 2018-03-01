4037502f057cf5d56e348a67db006ca0b2e4ceab
cmd-accname <mariusbauer91@gmail.com>
2018-03-01 20:22:10 +0100

    main asd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import Ride
+import auto
+from ride import Ride
 
 num_rows = 0;
 num_cols = 0;
@@ -8,25 +9,23 @@ bonus = 0;
 timesteps = 0;
 
 def read_input():
-    with open('a_example.in', 'r') as file:
+    with open('data/a_example.in', 'r') as file:
         content = file.readlines()
-        line1 = True
         rides = []
         for idx, line in enumerate(content):
+            line.rstrip()
+
             values = line.split(' ')
             if idx == 0:
-                num_rows = values[0]
-                num_cols = values[1]
-                num_vehicles = values[2]
-                num_rides = values[3]
-                bonus = values[4]
-                timesteps = values[5]
+                num_rows = int(values[0])
+                num_cols = int(values[1])
+                num_vehicles = int(values[2])
+                num_rides = int(values[3])
+                bonus = int(values[4])
+                timesteps = int(values[5])
             else:
-                rides.append(Ride())
-                print(line)
+                rides.append(Ride((int(values[0]), int(values[1])), (int(values[2]), int(values[3])), int(values[4]), int(values[5])))
+        return rides
 
-                print(values)
-
-
-read_input()
-
+def main():
+    rides = read_input()
