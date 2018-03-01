973427cf043ede21149a8261544048087c21cdf0
manu183 <manuellang183@gmail.com>
2018-03-01 20:52:11 +0100

    FUUUUU

--- auto.py
+++ auto.py
@@ -1,5 +1,5 @@
 class Auto:
-    def __init__(self, position=(0,0), is_driving = False, index, t_free = 0, rides_started = []):
+    def __init__(self, index, position=(0,0), is_driving = False, t_free = 0, rides_started = []):
         self.position = position
         self.is_driving = is_driving
         self.index = index
